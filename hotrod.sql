58afeadf8f097204464b4591ab85d97036ae1eb1
enzonami <160043231+enzonami@users.noreply.github.com>
2025-01-20 05:32:04 -0600

    Delete hotrod.sql

--- hotrod.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS vehicle_shake (
-    plate VARCHAR(20) NOT NULL PRIMARY KEY, -- License plate of the vehicle
-    applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp of when the shaky module was applied
-);
